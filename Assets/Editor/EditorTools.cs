0ed781159cdd09890a70fe59d9345f1047a4de39
xue-fei <awnuxcvbn@163.com>
2025-03-26 16:31:41 +0800

    编辑器生成游戏列表文件

--- Assets/Editor/EditorTools.cs
+++ Assets/Editor/EditorTools.cs
@@ -1,5 +1,9 @@
-﻿using UnityEditor;
+﻿using System.IO;
+using System;
+using UnityEditor;
 using UnityEngine;
+using NUnit.Framework;
+using System.Collections.Generic;
 
 public class EditorTools : MonoBehaviour
 {
@@ -7,5 +11,25 @@ public class EditorTools : MonoBehaviour
     static void OpenPersistentDataPath()
     {
         System.Diagnostics.Process.Start(@Application.persistentDataPath);
+    }
+
+    [MenuItem("工具/生成游戏列表")]
+    static void CreatGameListJson()
+    {
+        string path = Application.streamingAssetsPath + "/Game";
+        DirectoryInfo root = new DirectoryInfo(path);
+        GameList gameList = new GameList();
+        foreach (FileInfo f in root.GetFiles())
+        {
+            if (f.FullName.Contains(".meta"))
+            {
+                continue;
+            }
+            string name = Path.GetFileNameWithoutExtension(f.FullName);
+            gameList.Names.Add(name);
+            Debug.Log(name);
+        }
+        string json = JsonUtility.ToJson(gameList);
+        File.WriteAllText(Application.streamingAssetsPath + "/gamelist.json", json);
     }
 }
