0f058714a40f7fb28f9a7a493b9116aafc539e02
xue-fei <awnuxcvbn@163.com>
2025-08-31 22:01:51 +0800

    去掉地图偏移

--- Assets/Scripts/BBKRPGSimulator/Core/Definitions/Constants.cs
+++ Assets/Scripts/BBKRPGSimulator/Core/Definitions/Constants.cs
@@ -10,16 +10,14 @@ namespace BBKRPGSimulator
         public const int COLOR_BLACK = -16777216;
         public const int COLOR_TRANSP = 0;
         public const int COLOR_WHITE = -3356691;
-        public const int MAP_LEFT_OFFSET = 8;
 
         /// <summary>
-        /// 屏幕高度
-        /// 实际是96而非106？？？
+        /// 屏幕高度 96的倍数
         /// </summary>
         public const int SCREEN_HEIGHT = 96;
 
         /// <summary>
-        /// 屏幕宽度
+        /// 屏幕宽度 160的倍数
         /// </summary>
         public const int SCREEN_WIDTH = 160;
 
