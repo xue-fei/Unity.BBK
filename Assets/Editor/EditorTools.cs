fdeaa7e19622b71f297fe3c64779d4eabdce0569
xue-fei <awnuxcvbn@163.com>
2025-09-02 14:08:02 +0800

    编辑器打包WebGL代码

--- Assets/Editor/EditorTools.cs
+++ Assets/Editor/EditorTools.cs
@@ -1,5 +1,7 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using UnityEditor;
+using UnityEditor.Build.Reporting;
 using UnityEngine;
 
 public class EditorTools : MonoBehaviour
@@ -10,7 +12,7 @@ public class EditorTools : MonoBehaviour
         System.Diagnostics.Process.Start(@Application.persistentDataPath);
     }
 
-    [MenuItem("工具/生成游戏列表")]
+    //[MenuItem("工具/生成游戏列表")]
     static void CreatGameListJson()
     {
         string path = Application.streamingAssetsPath + "/Game";
@@ -28,5 +30,41 @@ public class EditorTools : MonoBehaviour
         }
         string json = JsonUtility.ToJson(gameList);
         File.WriteAllText(Application.streamingAssetsPath + "/gamelist.json", json);
+    }
+
+    static string rootPath;
+
+    [MenuItem("工具/打包WebGL")]
+    public static void RunBuild()
+    {
+        CreatGameListJson();
+
+        string nowTime = DateTime.Now.ToString("yyyy.MM.dd.HH.mm.ss");
+        rootPath = Application.dataPath + "/../Build/Build" + nowTime + "/";
+        if (!Directory.Exists(rootPath))
+        {
+            Directory.CreateDirectory(rootPath);
+        }
+        Build("bbkrpg-unity");
+        System.Diagnostics.Process.Start(Application.dataPath + "/../Build/");
+    }
+
+    public static void Build(string sceneName)
+    {
+        if (!Directory.Exists(rootPath + "bbk/"))
+        {
+            Directory.CreateDirectory(rootPath + "bbk/");
+        }
+        BuildReport br = BuildPipeline.BuildPlayer(new[] { "Assets/Scenes/" + sceneName + ".unity" },
+            rootPath + "bbk/", BuildTarget.WebGL, BuildOptions.None);
+        if (br.files.Length < 0)
+        {
+            throw new Exception("BuildPlayer failure: " + br.strippingInfo);
+        }
+        else
+        {
+            //DirectoryInfo directoryInfo = new DirectoryInfo(rootPath + "bbk/");
+            //directoryInfo.MoveTo(rootPath + sceneName + "/");
+        }
     }
 }
